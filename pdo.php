1ac5671e8d5e3d87c2ddbf0a37450c10815bce21
REYADUL <reyadul.islam792@gmail.com>
2023-03-06 22:58:32 +0600

    particular db-name not required

--- pdo.php
+++ pdo.php
@@ -1,20 +1,40 @@
 <?php
 
-    $dsn = 'mysql:host=localhost;dbname= mydb';
-
-    $username = 'root';
-    $password = '';
-
     try{
-        $connection = new PDO($dsn,$username,$password);
+        $connection = new PDO('mysql:host=localhost','root','');
+
+        var_dump($connection);
 
         $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     }
     catch(PDOException $exception){
         echo $exception->getMessage();
     }
+
+
+    // $connection = new mysqli('localhost','root','','userdb');
+    // if(! $connection){
+    //      var_dump($connection) ;
+    // }
+    // else echo 'connected !'. '<br><br>';
+   
+    // $link = mysqli_connect("localhost", "root", "",'userdb');
+ 
+    // if($link === false){
+    //     die("ERROR: Could not connect. " . mysqli_connect_error());
+    // }
+ 
+    // echo "Connect Successfully. Host info: " . mysqli_get_host_info($link).'<br><br>';
+
+
+    // $connection = new PDO('mysql:host=localhost','root','');
+    // var_dump($connection);
+
+
+
+
     
 
-    var_dump($connection);
-    echo 'ok';
+    
+   
 ?>
