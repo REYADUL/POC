83cd76b28f4713e81dc2143305eeb8fc69df665e
REYADUL <reyadul.islam792@gmail.com>
2023-03-13 14:30:41 +0600

    application url

--- mvc_pattern/public/index.php
+++ mvc_pattern/public/index.php
@@ -1,1 +1,13 @@
-index.php
+<?php   
+function show($stuff){
+    echo "<pre>";
+    print_r($stuff);
+    echo "</pre>";
+}
+
+$URL=$_GET['url']??'home';
+
+show($_GET);
+$URL= explode("/",$URL);
+show($URL);
+
---
+++ object.php
@@ -0,0 +1,22 @@
+<?php
+
+/**USE OF PDO --> php data object -which is built in class to do database operation by PHP  */
+
+/** PDO gives protection sql injectoion and also sql attack  */
+
+/**PDO support many drivers upto 10 and many more   */
+
+PDO method:
+
+/**CONSTRUCT method 
+ * 
+ * dsn ->data source name (it take data as string)
+ * 
+ */
+// $connection = new mysqli('localhost','root','','userdb');
+
+//$connection = new PDO($dsn,$username,$password);
+
+
+
+?>
