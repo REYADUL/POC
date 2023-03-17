a0dca1fab20ba1a4dcdbdb7ecf72af12d85a2c21
REYADUL <reyadul.islam792@gmail.com>
2023-03-17 22:14:02 +0600

    call_user_func_array()--not need

--- mvc_pattern/application/controllers/Home.php
+++ mvc_pattern/application/controllers/Home.php
@@ -1,1 +1,14 @@
-home controller page
+<?php
+class Home extends Controller
+{
+    public function index(){
+        // echo 'this is the home controller <br>pp';
+
+        $this->view('home');
+    }
+}
+
+// $home = new Home();
+// call_user_func_array([$home,'index'],[])
+
+?>
