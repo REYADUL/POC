df15d31c48b8e65d93123b801f617a83b59f3e98
REYADUL <reyadul.islam792@gmail.com>
2023-03-17 21:56:09 +0600

    add asset folder

--- mvc_pattern/application/controllers/Products.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo "<h1 style='color: #468468;text-align:center;'>Your Desired Products</h1>";
-    echo '<img src="https://girlbd.files.wordpress.com/2015/04/55.jpg" alt="Girl in a jacket" width="500" height="400">';
